b8e8af1c3b947b3e631686eebb781b7913b0f46c
Ivan Indrakusuma <ivan.17051@mhs.its.ac.id>
2019-05-12 22:52:59 +0700

    Tambah thread

--- FP.c
+++ FP.c
@@ -2,54 +2,18 @@
 #include<stdlib.h>
 #include<time.h>
 #include<string.h>
+#include<pthread.h>
+#include<unistd.h>
 
+
+pthread_t tid1, tid2;
 int flag1, flag2, flag3, flag4, flag5;
+char buff[255], waktu[2], cmd[50], temp[100];
+int time2[5];
 
-int menit(int x, int y){
-	if(x==y){
-		return flag1=1;
-	}
-	else return flag1=0;
-}
-
-int jam2(int x, int y){
-	if(x==y){
-		return flag2=1;
-	}
-	else return flag2=0;
-}
-
-int hari1(int x, int y){
-	if(x==y){
-		return flag3=1;
-	}
-	else return flag3=0;
-}
-
-int bulan(int x, int y){
-	if(x==y){
-		return flag4=1;
-	}
-	else return flag4=0;
-}
-
-int hari2(int x, int y){
-	if(x==y){
-		return flag5=1;
-	}
-	else return flag5=0;
-}
-
-int main(void) {
-	time_t waktu;
-	struct tm *jam;
-	time(&waktu);
-	jam = localtime(&waktu);
-	printf("Atime:%04d-%02d-%02d_%02d:%02d:%02d\n", jam->tm_year+1900, jam->tm_mon+1, jam->tm_mday, jam->tm_hour, jam->tm_min, jam->tm_sec);
-	
-	int i, j, time2[5], a, x;
+void* baca(){
+	int i, j, a, x;
    	FILE *fp;
-   	char buff[255], time[2], cmd[50], temp[100];
    	fp = fopen("crontab.data", "r");
    	if(fp==NULL){
    		perror("Error while opening the file.\n");
@@ -65,13 +29,13 @@ int main(void) {
 				//printf("a1=%d\n", a);
 			}
 			else {
-				time[i++]=buff[j];
+				waktu[i++]=buff[j];
 				if(buff[j+1]==' '){
-					time2[a++]=atoi(time);
+					time2[a++]=atoi(waktu);
 					//printf("a2=%d\n", a);
-					time[0]=0;
-					time[1]=0;
-					time[2]=0;
+					waktu[0]=0;
+					waktu[1]=0;
+					waktu[2]=0;
 					i=0;
 				}	
 			}	
@@ -100,22 +64,100 @@ int main(void) {
 		}
 	}
 	strcat(cmd, temp);
-	
+	for(i=0;i<5;i++) printf("%d ", time2[i]);
+	printf("\n%s\n", cmd);
+	fclose(fp);
+}
 
-	//for(i=0;i<5;i++) printf("%d ", time2[i]);
-	//printf("\n%s\n", cmd);
+void* cek(){
+
+	time_t waktu;
+	struct tm *jam;
+	time(&waktu);
+	jam = localtime(&waktu);
+	printf("Atime:%04d-%02d-%02d_%02d:%02d:%02d\n", jam->tm_year+1900, jam->tm_mon+1, jam->tm_mday, jam->tm_hour, jam->tm_min, jam->tm_sec);
+
+	//Menit
+	if(time2[0]==-1){
+		flag1=1;
+	}
+	else if(time2[0]!=-1){
+		if(time2[0]<0 || time2[0]>59)
+      		exit(EXIT_FAILURE);
+		else if(jam->tm_min==time2[0])
+			flag1=1;
+		else flag1=0;
+	}
+	
+	//Jam
+	if(time2[1]==-1){
+		flag2=1;
+	}
+	else if(time2[1]!=-1){
+		if(time2[1]<0 || time2[1]>23)
+      		exit(EXIT_FAILURE);
+		else if(jam->tm_hour==time2[1])
+			flag2=1;
+		else flag2=0;
+	}
+	
+	//Hari(Tanggal)
+	if(time2[2]==-1){
+		flag3=1;
+	}
+	else if(time2[2]!=-1){
+		if(time2[2]<1 || time2[2]>31)
+      		exit(EXIT_FAILURE);
+		else if(jam->tm_mday==time2[2])
+			flag3=1;
+		else flag3=0;
+	}
+	
+	//Bulan
+	if(time2[3]==-1){
+		flag4=1;
+	}
+	else if(time2[3]!=-1){
+		if(time2[3]<1 || time2[3]>12)
+      		exit(EXIT_FAILURE);
+		else if(jam->tm_mon==time2[3])
+			flag4=1;
+		else flag4=0;
+	}
+	
+	//Hari
+	if(time2[4]==-1){
+		flag5=1;
+	}
+	else if(time2[4]!=-1){
+		if(time2[4]==7){
+			time2[4]=0;
+			if(time2[4]<0 || time2[4]>7)
+      			exit(EXIT_FAILURE);
+			else if(jam->tm_wday==time2[4])
+				flag5=1;
+			else flag5=0;
+		}
+	}
+   	
+}
+
+
+int main(void) {
+	
+	pthread_create(&(tid1), NULL, (void*)baca, NULL);
+    pthread_join(tid1, NULL);
+
+    pthread_create(&(tid2), NULL, (void*)cek, NULL);
+    pthread_join(tid2, NULL);
+	
 	//printf("%s\n", buff);
    	
    	//if(time2[1]==jam->tm_min) printf("Bisa1\n");
-   	menit(jam->tm_min, time2[0]);
-   	jam2(jam->tm_hour, time2[1]);
-   	hari1(jam->tm_mday, time2[2]);
-   	bulan(jam->tm_mon, time2[3]);
-   	hari2(jam->tm_wday, time2[4]);
-   	//printf("%d-%d-%d-%d-%d\n",flag1, flag2, flag3, flag4, flag5);
+   	
+   	printf("%d-%d-%d-%d-%d\n",flag1, flag2, flag3, flag4, flag5);
    	if(flag1==1 && flag2==1 && flag3==1 && flag4==1 && flag5==1)system(cmd);
-
-   	fclose(fp);
+   	
 	return 0;
 }
 
